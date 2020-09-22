cad72455d6238d4ab57bcde4b0bf1a5d8def307f
kensan-a <a.muramatu@gmail.com>
2020-09-22 04:04:25 +0000

    202009221304

--- server/server.js
+++ server/server.js
@@ -1,37 +1,8 @@
 const express = require("express");
-// const cors = require("cors");
 const RssParser = require("rss-parser");
 
 const app = express();
-
 app.use(express.static('public'));
-
-// app.use(cors({ origin: true, credentials: true }));
-
-// app.use(cors())
-
-// const allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Content-Type, Authorization, access_token'
-//   )
-
-//   // intercept OPTIONS method
-//   if ('OPTIONS' === req.method) {
-//     res.send(200)
-//   } else {
-//     next()
-//   }
-// }
-// app.use(allowCrossDomain)
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 const port = 8080;
 
@@ -43,8 +14,6 @@ const getFeeds = async (url) => {
   return feeds;
 };
 
-// app.get('/', cors(), async (req, res, next) => {
-// app.get('/', async (req, res, next) => {
 app.get('/rss-feed', async (req, res) => {
 
   const url = req.query.url;
@@ -56,20 +25,6 @@ app.get('/rss-feed', async (req, res) => {
     console.log(e);
   }
   console.log(feeds);
-
-  // res.header('Access-Control-Allow-Origin', 'https://5000-cs-994772306133-default.asia-east1.cloudshell.dev');
-  // // res.header('Access-Control-Allow-Origin', '*');
-  
-  // res.header('Access-Control-Allow-Credentials', true);
-
-  // res.header('Access-Control-Allow-Methods', 'PUT,DELETE,PATCH');
-  // // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-   
-  // res.header(
-  //   'Access-Control-Allow-Headers',
-  //   // 'Content-Type, Authorization, access_token',
-  //   "X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept",
-  // )
 
   res.json(feeds);
 });
--- src/api/rssFeedProxy.ts
+++ src/api/rssFeedProxy.ts
@@ -1,26 +1,15 @@
-const getFeeds = async (rssUrl: string) => {
+export const getFeeds = async (rssUrl: string) => {
   const url = "https://8080-cs-994772306133-default.asia-east1.cloudshell.dev/rss-feed/";
   
   const query = `url=${rssUrl}`;
-  const input = `${url}?${query}`;
-  console.log(input);
+  const inputRow = `${url}?${query}`;
+  console.log(inputRow);
 
-  const encodedInput = encodeURI(input);
+  const input = encodeURI(inputRow);
 
-  // const init: RequestInit = {
-  //   method: "GET",
-  //   mode: "cors",
-  //   credentials: "include",
-  //   headers: {
-  //     "Content-Type": "application/json; charset=utf-8",
-  //     "Accept": "application/vnd.github.mercy-preview+json",
-  //   },
-  // };
+  const response = await fetch(input);
+  const feeds = await response.json();
+  console.log(feeds);
 
-  // const response = await fetch(encodedInput, init);
-  const response = await fetch(encodedInput);
-  const myJson = await response.json();
-  console.log(myJson);
+  return feeds;
 };
-
-export { getFeeds };
