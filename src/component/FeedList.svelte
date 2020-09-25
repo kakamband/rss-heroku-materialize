e206df73642bff525c2ce3b2241877a04acf708e
kensan-a <a.muramatu@gmail.com>
2020-09-25 21:12:24 +0000

    202009260612

--- src/component/FeedList.svelte
+++ src/component/FeedList.svelte
@@ -16,20 +16,3 @@
   </fieldset>
 </form>
 {/each}
- 
-<ul>
-  {#each feeds as feed}
-  <li>
-    <a href={feed.url}>{feed.url}</a>
-    <ul>
-      {#each feed.contents as content}
-      <li>
-        {content.title}
-        {content.isoDate}
-        <a href={content.link}>{content.link}</a>
-      </li>
-      {/each}
-    </ul>
-  </li>
-  {/each}
-</ul>
