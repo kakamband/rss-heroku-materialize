6f879b9b3cc0c7a32462eb11edacba0cab0361bf
kensan-a <a.muramatu@gmail.com>
2020-09-27 06:05:55 +0000

    取得するRSSフィードのURLを登録を実装 #4

--- src/component/FeedInfo.svelte
+++ src/component/FeedInfo.svelte
@@ -4,8 +4,12 @@
   export let feedUrls: string[] = [];
   const dispatch = createEventDispatcher();
 
-  const exec = () => {
-		dispatch("exec");
+  const add = () => {
+		dispatch("exec", { payload: "add" });
+  };
+
+  const confirm = () => {
+		dispatch("exec", { payload: "confirm" });
   };
 </script>
 
@@ -14,5 +18,6 @@
   <input type="url" name={i} required bind:value={feedUrl}>
   {/each}
 
-  <input type="button" value="確定" on:click={exec}>
+  <input type="button" value="追加" on:click={add}>
+  <input type="button" value="確定" on:click={confirm}>
 </form>
