fccc93ffa4a332e849846f5f9f514b894ddd1275
kensan-a <a.muramatu@gmail.com>
2020-10-03 02:58:20 +0000

    202010031158

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const RssParser = require("rss-parser");
 
 // const port = 8080;
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static("public"));
