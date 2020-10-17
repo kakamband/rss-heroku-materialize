2ae6063acfb25246dbc4639a5c02835f4f06f081
kensan-a <a.muramatu@gmail.com>
2020-10-17 03:54:43 +0000

    202010171254 #12

--- src/component/App.svelte
+++ src/component/App.svelte
@@ -13,7 +13,64 @@
   let feedInfos: IfeedInfo[] = [];
   let feeds: Ifeed[] = [];
 
+  let authUi = null;
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKxOAhXymGjUrtiodvue3xL7WA16qd9cc",
+    authDomain: "rss-feed-proxy.firebaseapp.com",
+    // databaseURL: "https://rss-feed-proxy.firebaseio.com",
+    projectId: "rss-feed-proxy",
+    // storageBucket: "rss-feed-proxy.appspot.com",
+    // messagingSenderId: "1090474250814",
+    appId: "1:1090474250814:web:6a5631b43bc8b5e13d376f"
+  };
+
+  const uiConfig = {
+    // callbacks: {
+    //   signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    //     // User successfully signed in.
+    //     // Return type determines whether we continue the redirect automatically
+    //     // or whether we leave that to developer to handle.
+    //     return true;
+    //   },
+    //   uiShown: function() {
+    //     // The widget is rendered.
+    //     // Hide the loader.
+    //     document.getElementById('loader').style.display = 'none';
+    //   }
+    // },
+    // // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    // signInFlow: 'popup',
+    signInSuccessUrl: `${location.origin}/`,
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+  const onAuthStateChanged = (user) => {
+    if (user) {
+      console.log(user.uid, user.email);
+    } else {
+      console.log("User not logged in.");
+      authUi.start('#firebaseui-auth-container', uiConfig);
+    }
+  };
+
   onMount(async () => {
+    firebase.initializeApp(firebaseConfig);
+    firebase.auth().onAuthStateChanged(onAuthStateChanged, (e) => {
+      console.log(e);
+    });
+    authUi = new firebaseui.auth.AuthUI(firebase.auth());
+    // if (authUi.isPendingRedirect()) {
+    //   authUi.start('#firebaseui-auth-container', uiConfig);
+    // }
+
     feeds = await getFeeds(feedInfos);
   });
 
@@ -65,6 +122,9 @@
   </Router>
   {/if}
 </main>
+
+<div id="firebaseui-auth-container"></div>
+<input type="button" value="SignOut" on:click={() => firebase.auth().signOut()}>
 
 <svelte:head>
 	<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css">
