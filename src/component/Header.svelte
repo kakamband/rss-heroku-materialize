2866b8b1889e161c9b0af72c0bdaf13be62d56fe
kensan-a <a.muramatu@gmail.com>
2020-10-25 02:37:07 +0000

    ヘッダーを改良 #2

--- src/component/Header.svelte
+++ src/component/Header.svelte
@@ -5,9 +5,31 @@
 
 <header class="navbar is-primary is-fixed-top">
   <div class="navbar-brand">
-    <span class="navbar-item">
-      <i class="fas fa-rss"></i>
+    <span class="navbar-item dropdown is-hoverable">
+      <a href={"#"} class="dropdown-trigger navbar-link is-arrowless">
+        <i class="fas fa-rss"></i>
+      </a>
+      
+      <div class="dropdown-menu">
+        <div class="dropdown-content">
+          <span class="dropdown-item">Dropdown item</span>
+          <hr class="dropdown-divider">
+          <span class="dropdown-item">With a divider</span>
+        </div>
+      </div>
     </span>
+
+    <!-- <span class="navbar-item has-dropdown is-hoverable">
+      <a href={"#"} class="navbar-link is-arrowless">
+        <i class="fas fa-rss"></i>
+      </a>
+
+      <div class="navbar-dropdown">
+        <span class="navbar-item">Dropdown item</span>
+        <hr class="navbar-divider">
+        <span class="navbar-item">With a divider</span>
+      </div>
+    </span> -->
   </div>
 
   <nav class="navbar-menu navbar-end">
