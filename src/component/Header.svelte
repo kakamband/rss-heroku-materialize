765d299334d39630dcefdd56720f7fb4065ac150
kensan-a <a.muramatu@gmail.com>
2020-10-31 02:01:58 +0000

    202010311102

--- src/component/Header.svelte
+++ src/component/Header.svelte
@@ -4,43 +4,37 @@
 
 <Router>
   <ul id="dropdown1" class="dropdown-content">
-    <li class="divider"></li>
     <li>
-      <Link to="/">Feed</Link>
+      <Link to="/"><i class="material-icons">rss_feed</i></Link>
     </li>
     <li>
-      <Link to="/feed-info">設定</Link>
+      <Link to="/feed-info"><i class="material-icons">settings</i></Link>
     </li>
   </ul>
 </Router>
 
 <div class="navbar-fixed">
-<nav>
-  <div class="nav-wrapper">
-    <a href="#!" class="brand-logo left">Logo</a>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo left dropdown-trigger" data-target="dropdown1">
+        <i class="material-icons">menu</i>
+      </a>
 
-    <Router>
-      <ul class="hide-on-med-and-down left">
-        <li>
-          <Link to="/">Feed</Link>
-        </li>
-        <li>
-          <Link to="/feed-info">設定</Link>
-        </li>
-      </ul>
-    </Router>
+      <Router>
+        <ul class="right">
+          <li class="hide-on-small-only">
+            <Link to="/"><i class="material-icons">rss_feed</i></Link>
+          </li>
+          
+          <li class="hide-on-small-only">
+            <Link to="/feed-info"><i class="material-icons">settings</i></Link>
+          </li>
 
-    <ul class="right">
-      <li>
-        <a class="dropdown-trigger" href="#!" data-target="dropdown1">
-          <i class="material-icons right">more_vert</i>
-        </a>
-      </li>
-
-      <li>
-        <slot name="auth"></slot>
-      </li>
-    </ul>
-  </div>
-</nav>
+          <li>
+            <slot name="auth"></slot>
+          </li>
+        </ul>
+      </Router>
+    </div>
+  </nav>
 </div>
