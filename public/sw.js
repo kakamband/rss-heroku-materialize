42305f73017d51d7149cd06ff34b1147a9432ed6
kensan-a <44194688+kensan-a@users.noreply.github.com>
2020-11-03 15:21:57 +0900

    202011031521

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,23 @@
-self.addEventListener("install", (e) => {
+// キャッシュファイルの指定
+const CACHE_NAME = "rss-feed-app-caches";
+const urlsToCache = ["index.html", "/build/bundle.js", "/bundle.css"];
+
+// インストール処理
+self.addEventListener("install", (event) => {
   console.log("[Service Worker] Install");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response ? response : fetch(event.request);
+    })
+  );
 });
