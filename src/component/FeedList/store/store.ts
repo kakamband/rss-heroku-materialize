1c639cb74f447878ff04c915888ff55e0f00e7d7
kensan-a <44194688+kensan-a@users.noreply.github.com>
2020-11-19 12:11:12 +0900

    initial commit

--- src/component/FeedList/store/store.ts
+++ src/component/FeedList/store/store.ts
@@ -1,6 +1,6 @@
 import { writable, get } from "svelte/store";
 import type { Ifeed, IfeedInfo } from "../../../common/Feed";
-import { getFeeds } from "../../../api/rssFeedProxy";
+import { getFeed, getFeeds } from "../../../api/rssFeedProxy";
 
 interface IfeedStore {
   feeds: Ifeed[];
@@ -12,8 +12,20 @@ const initialState: IfeedStore = {
 const { subscribe, set, update } = writable(initialState);
 
 const load = async (feedInfos: IfeedInfo[]) => {
-  const loaded = await getFeeds(feedInfos);
-  update((n) => ({ ...n, feeds: loaded }));
+  // const loaded = await getFeeds(feedInfos);
+  // update((n) => ({ ...n, feeds: loaded }));
+
+  update((n) => ({ ...n, feeds: [] }));
+
+  for (const [i, feedInfo] of feedInfos.entries()) {
+    const feed = await getFeed(feedInfo.url);
+    update((n) => {
+      const before = n.feeds.length;
+      n.feeds[i] = feed;
+      console.log(before, "->", n.feeds.length);
+      return { ...n };
+    });
+  }
 };
 
 const clear = () => set(initialState);
